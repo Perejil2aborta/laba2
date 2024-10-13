0739518b799f6fcccaf4519f172834c2e365d746
Perejil2aborta <danil.topilin.95@gmail.com>
2024-10-13 20:38:44 +0500

    Create 3.py

---
+++ homework3/3.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+
+def calculate_age():
+    while True:
+        birth_date = input("Введите дату рождения (дд/мм/гггг): ")
+        try:
+            day, month, year = map(int, birth_date.split('/'))
+            # Проверка на корректность даты
+            birth_date = datetime(year, month, day)
+            break  # Выход из цикла, если дата корректна
+        except ValueError:
+            print("Некорректный ввод. Пожалуйста, введите дату в формате дд/мм/гггг.")
+
+    today = datetime.now()
+    age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+
+    print(f"Ваш возраст: {age} лет")
+
+
+calculate_age()
